adb8bd1496d73b286c6d27f08831031e523fb360
Gonzalo Gestri <gonzalogestri92299@gmail.com>
2023-12-12 09:08:28 -0300

    configuracion de rutas iniciales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './simpsons/home/home.component';
+import { ListComponent } from './simpsons/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        title:'Simpsons',
+        component: HomeComponent,
+    },
+    {
+        path: 'list',
+        title:'Simpsons List',
+        component: ListComponent,
+    },
+    {
+        path: '**',
+        title:'Simpsons',
+        redirectTo: '/home'
+    },
+
+];
