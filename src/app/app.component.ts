7076e4165845ff30775e4a9b85d5d16947231bc7
Gonzalo Gestri <gonzalogestri92299@gmail.com>
2023-12-12 09:42:32 -0300

    implmentacion del nav como componente general

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './shared/components/nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavComponent],
   template: `
+    <app-nav />
     <router-outlet />
   `,
-  styles: ``
+  styles: `
+    app-nav {
+      position: fixed;
+      width: 100%;
+    }
+  `
 })
 export class AppComponent {
   title = 'app-version17';
